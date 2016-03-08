b24471fb5132600a6e29ce64e702c6d7a2fea0ff
Matt Jenkins <matt@majenko.co.uk>
2016-03-08 11:56:35 +0000

    Initial import

---
+++ PMOD_Shield.h
@@ -0,0 +1,138 @@
+#ifndef _PMOD_SHIELD_H
+#define _PMOD_SHIELD_H
+
+// J1 pins
+#define J1_01            8
+#define J1_02           34
+#define J1_03            9
+#define J1_04           35
+#define J1_05           10
+#define J1_06           36
+#define J1_07           11
+#define J1_08           37
+#define J1_09           12
+#define J1_10           38
+#define J1_11           13
+#define J1_12           39
+//#define J1_13           -1
+#define J1_14           40
+#define J1_15           42
+#define J1_16           41
+
+// J2 pins (I2C)
+#define J2_01           37
+#define J2_02           37
+#define J2_03           36
+#define J2_04           36
+
+// J3 pins
+#define J3_01            0
+#define J3_02           26
+#define J3_03            1
+#define J3_04           27
+#define J3_05            2
+#define J3_06           28
+#define J3_07            3
+#define J3_08           29
+#define J3_09            4
+#define J3_10           30
+#define J3_11            5
+#define J3_12           31
+#define J3_13            6
+#define J3_14           32
+#define J3_15            7
+#define J3_16           33
+
+// J4 pins
+#define J4_01           14
+#define J4_02           20
+#define J4_03           15
+#define J4_04           21
+#define J4_05           16
+#define J4_06           22
+#define J4_07           17
+#define J4_08           23
+#define J4_09           18
+#define J4_10           24
+#define J4_11           19
+#define J4_12           25
+
+// J6 pins
+#define J6_01            5
+//#define J6_02           -1
+#define J6_03           13
+#define J6_04            6
+#define J6_05           10
+//#define J6_06           -1
+
+// JA pins
+#define JA_01           26
+#define JA_02           27
+#define JA_03           28
+#define JA_04           29
+//#define JA_05           -1
+//#define JA_06           -1
+#define JA_07           30
+#define JA_08           31
+#define JA_09           32
+#define JA_10           33
+//#define JA_11           -1
+//#define JA_12           -1
+
+// JB pins
+#define JB_01            7
+#define JB_02            1
+#define JB_03            0
+#define JB_04           38
+//#define JB_05           -1
+//#define JB_06           -1
+#define JB_07           36
+#define JB_08           34
+#define JB_09           10
+#define JB_10           37
+//#define JB_11           -1
+//#define JB_12           -1
+
+// JC pins
+#define JC_01           10
+//#define JC_02           -1
+//#define JC_03           -1
+#define JC_04           13
+//#define JC_05           -1
+//#define JC_06           -1
+#define JC_07           2
+#define JC_08           21
+#define JC_09           15
+#define JC_10           20
+//#define JC_11           -1
+//#define JC_12           -1
+
+// JD pins
+#define JD_01           16
+#define JD_02           40
+#define JD_03           39
+#define JD_04           19
+//#define JD_05           -1
+//#define JD_06           -1
+#define JD_07           42
+#define JD_08           41
+#define JD_09            3
+#define JD_10            5
+//#define JD_11           -1
+//#define JD_12           -1
+
+// JE pins
+#define JE_01           14
+#define JE_02            6
+#define JE_03            8
+#define JE_04           22
+//#define JE_05           -1
+//#define JE_06           -1
+#define JE_07           18
+#define JE_08            9
+#define JE_09           35
+#define JE_10           24
+//#define JE_11           -1
+//#define JE_12           -1
+
+#endif
